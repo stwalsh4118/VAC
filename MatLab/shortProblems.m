294c9d385724c192a95d05469cead774fcb009af
stwalsh <33824192+Pr0grammingPanda@users.noreply.github.com>
2018-06-11 22:57:40 -0500

    shortProblems homework 6_11_18
    letter z of number 4 not done

---
+++ MatLab/shortProblems.m
@@ -0,0 +1,23 @@
+%% 1)
+a = 10;
+b = (2.5)*10^23;
+c = 2+3*i;
+d = exp(1)^(j*2*(pi/3));
+
+%% 4)
+x = 1/(1+exp(-(a-15)/6));
+y = (sqrt(a)+(b^(1/21)))^pi;
+%z ??? lol
+
+%% 8)
+figure;
+hold on;
+t = linspace(0,2*pi,1000);
+plot(t,sin(t));
+plot(t,cos(t), '--r');
+xlabel('Time(s)');
+ylabel('Function Value');
+title('Sin and Cos Functions');
+legend('Sin','Cos');
+
+
