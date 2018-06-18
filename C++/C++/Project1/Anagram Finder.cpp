d5214f79ca10b8100bbd6d2d514fb228945451cf
stwalsh <33824192+Pr0grammingPanda@users.noreply.github.com>
2018-06-18 10:56:55 -0500

    C++ 6/18/2018

---
+++ C++/C++/Project1/Anagram Finder.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool anagramFind(string a, string b) {
+	bool result = true;
+	char c[100];
+	char d[100];
+	strcpy_s(c, a.c_str());
+	strcpy_s(d, b.c_str());
+	string C = "";
+	string D = "";
+	char temp;
+	for (int i = 0; i<a.length() - 1; i++){
+		for (int j = i + 1; j<a.length(); j++){
+			if (c[i] > c[j]){
+				temp = c[i];
+				c[i] = c[j];
+				c[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i<b.length() - 1; i++) {
+		for (int j = i + 1; j<b.length(); j++) {
+			if (d[i] > d[j]) {
+				temp = d[i];
+				d[i] = d[j];
+				d[j] = temp;
+			}
+		}
+	}
+	C = c;
+	D = d;
+	if (C != D)
+		result = false;
+	return result;
+}
+
+int main() {
+	string a;
+	string b;
+	cout << "What 2 words do you want to check if theyre anagrams? " << endl;
+	cin >> a;
+	cout << endl;
+	cin >> b;
+	if (a.length() == b.length())
+		if (anagramFind(a, b))
+			cout << "The words entered are anagrams.";
+		else
+			cout << " the words are not anagrams.";
+	else
+		cout << "The words entered are not anagrams.";
+	cin >> a;
+	
+
+
+	
+
+	return 0;
+}
